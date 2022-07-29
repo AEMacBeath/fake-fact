7b6c2ea73d537d95bf3ef952af1dcf974953e338
AEMacBeath <alicemacbeath94@gmail.com>
2022-07-29 07:09:42 +0000

    Deployment commit to correct missing CSS in Heroku.

--- fakefact/settings.py
+++ fakefact/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["fakefact2022.herokuapp.com", "localhost"]
 
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
-        'cloudinary_storage',
-        'django.contrib.staticfiles',
-        'cloudinary',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',

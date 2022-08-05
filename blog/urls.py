854d22442ec7d8aecb4e4bd98a0c80da162aa481
AEMacBeath <alicemacbeath94@gmail.com>
2022-08-05 17:23:24 +0000

    Added admin panel link to navbar when signed in as superuser.

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('admin/', views.PostList.as_view(), name='admin'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('fake/<slug:slug>', views.PostFakeVote.as_view(), name='post_fake'),
-    path('fact/<slug:slug>', views.PostFactVote.as_view(), name='post_fact'),
+    path('fake/<slug:slug>/', views.PostFakeVote.as_view(), name='post_fake'),
+    path('fact/<slug:slug>/', views.PostFactVote.as_view(), name='post_fact'),
     path('message/<int:pk>/update/', views.MessageUpdateView.as_view(), name='message_update'),
     path('message/<int:pk>/delete/', views.MessageDeleteView.as_view(), name='message_delete'),
 ]
